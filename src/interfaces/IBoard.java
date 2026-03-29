58b1e36c1ff9258e293da2a78585c7c98a0d5d61
Yashwanth Choudary Annem <Yashwanth.a.2025@computing.smu.edu.sg>
2026-03-29 23:44:39 +0800

    auto: add 1 new file(s) [2026-03-29 23:44:39]

---
+++ src/interfaces/IBoard.java
@@ -0,0 +1,22 @@
+package interfaces;
+
+import java.util.List;
+import java.util.Map;
+import model.Card;
+import model.Noble;
+import model.Token;
+
+public interface IBoard {
+    void refillMarket();
+    Map<Token, Integer> getAvailableTokens();
+    void removeToken(Token t, int count);
+    void addToken(Token t, int count);
+    Card[] getVisibleCards(int level);
+    List<Noble> getNobles();
+    void removeNoble(Noble n);
+    Card takeCard(int level, int index);
+    boolean canTakeTwo(Token t);
+    boolean canTakeThreeDifferent(Token t1, Token t2, Token t3);
+    Card drawFromDeck(int level);
+    boolean deckHasCards(int level);
+}
