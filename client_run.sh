bf82c741143daf65bd348b34a9786f362c9bd41c
Yashwanth Choudary Annem <Yashwanth.a.2025@computing.smu.edu.sg>
2026-03-30 10:40:40 +0800

    auto: update 1 file(s) [2026-03-30 10:40:39]

--- client_run.sh
+++ client_run.sh
@@ -1,2 +1,2 @@
-javac -d classes -cp src src/network/ClientMain.java
+javac -d classes -cp src src/interfaces/*.java src/model/*.java src/network/ClientMain.java
 java -cp classes network.ClientMain
