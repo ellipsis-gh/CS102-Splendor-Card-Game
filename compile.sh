84be319f14451dfded09e4012cff7b8c1747c04e
woshiniaoshen <tannelson2811@gmail.com>
2026-04-02 23:19:03 +0800

    edit compile file

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
-javac -d classes -cp "src:lib/jansi-2.4.1.jar" src/Main.java
-javac -d classes -cp "src:lib/jansi-2.4.1.jar" src/network/ServerMain.java
+javac -d classes -cp "src:lib/jansi-2.4.1.jar" src/network/ServerMain.java src/network/ClientMain.java src/Main.java
+
 echo "Compilation successful"
